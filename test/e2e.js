75fd623aafdcc51a50b1d2bba0cfe0e8c2611d37
Adam R. Wilczek <arwilczek90@gmail.com>
2017-01-22 16:44:56 -0600

    fixing test description

--- test/e2e.js
+++ test/e2e.js
@@ -62,13 +62,13 @@ describe('main window', function spec() {
     expect(await client.element('[data-qa="MainAppMenu::settingsButton"]')).to.exist;
   });
 
-  it('it should navigate to the next page when clicking on project button and load all three buttons', async () => {
+  it('should navigate to the next page when clicking on project button and load all three buttons', async () => {
     const { client } = this.app;
     client.waitUntilWindowLoaded();
     await client.click('[data-qa="MainAppMenu::menuButton"]');
     await delay(500);
     await client.click('[data-qa="MainAppMenu::projectsButton"]');
-    await expect(await client.getUrl()).to.include("/editor/projects");
+    await expect(await client.getUrl()).to.include('/editor/projects');
     await expect(await client.element('[data-qa="ProjectSelectorButtons::NewProject"]')).to.exist;
     await expect(await client.element('[data-qa="ProjectSelectorButtons::OpenProject"]')).to.exist;
     await expect(client.element('[data-qa="ProjectSelectorButtons::ReturnHome"]')).to.exist;
